60ca4b98abaab210b99b8efcd6ab18da47425744
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-10 10:38:16 +0100

    removed secret keys

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 import 'antd/dist/antd.css';
+import store from './app/store';
+
+import { Provider } from 'react-redux';
+
+require('dotenv').config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,27 @@
+// import axios from "axios";
+
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': process.env('RAPID_API_KEY'),
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+export const cryptoapi = createApi({
+    reducerPath: 'crypto',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => '/exchanges'
+        })
+    })
+});
