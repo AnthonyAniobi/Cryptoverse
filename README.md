61f2566f60ba339717c9ae67a1e7b958a4c67390
Anthony Aniobi <anthonyaniobi198@gmail.com>
2022-07-10 10:07:14 +0100

    edited readme and homepage

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Cryptoverse
 
 This is a react project for viewing and tracking crypto markets as well as the news associated with all cryptocurrencies.
+
+## Technologies and libraries used
+- React
+- Css
+- ant design
+- redux toolkit
+- axios
+- chart.js
+- html-react-parser
+- millify
+- moment
+- react
+- react-chartjs-2
+- react-redux
+- react-router-dom
+
+## Screenshots
--- src/App.js
+++ src/App.js
@@ -23,9 +23,18 @@ function App() {
             </Routes>
           </div>
         </Layout>
-      </div>
-      <div className='footer'>
 
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: "white", textAlign: "center" }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
